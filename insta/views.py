08bf16d264b57ce4946967f01f99a0a4e539a74a
edumorris <eduuwachira@gmail.com>
2020-08-09 12:22:19 +0300

    django-registration templates effected

--- insta/views.py
+++ insta/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+@login_required(login_url='/accounts/login')
 def home(request):
+    return render()
+
+@login_required(login_url='/accounts/login')
+def profile(request):
     return render()
